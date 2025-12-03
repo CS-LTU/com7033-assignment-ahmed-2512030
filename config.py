4c358254717106145e772231a52c474ba539ac7b
Ahmed <2512030@leedstrinity.ac.uk>
2025-12-03 22:27:14 +0500

    Structure updates

--- config.py
+++ config.py
@@ -4,32 +4,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'RANDOM-SECRET-KEY'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-this-key-secure-random-string'
     
-    # SQLAlchemy
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # MongoDB
     MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27018/stroke_app'
 
-    # Security
-    SESSION_COOKIE_SECURE = os.environ.get('FLASK_ENV') == 'production' # Only True in prod with HTTPS
+    SESSION_COOKIE_SECURE = os.environ.get('FLASK_ENV') == 'production'
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = 'Lax'
     
-    # Fernet key must be 32 url-safe base64-encoded bytes.
-    ENCRYPTION_KEY = os.environ.get('ENCRYPTION_KEY') or b'8co-7-z1x4-7-z1x4-7-z1x4-7-z1x4-7-z1x4-7-z1x4='
+    ENCRYPTION_KEY = os.environ.get('ENCRYPTION_KEY') or 'iJkN6XPB2A6vGQPIUqd-zr1Jfz6XQ7mnuoqQc66feME='
 
-    # 2FA
     WTF_CSRF_ENABLED = True
     
-    # Talisman
     FORCE_HTTPS = os.environ.get('FLASK_ENV') == 'production'
     
-    # Content Security Policy
     CSP = {
         'default-src': "'self'",
         'script-src': ["'self'", 'https://cdn.tailwindcss.com', 'https://cdnjs.cloudflare.com', 'https://cdn.jsdelivr.net', "'unsafe-inline'"],
